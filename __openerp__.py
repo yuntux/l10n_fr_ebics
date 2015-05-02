5c5240f9ad013464e6a9d8eb3e3775f6623f084d
yuntux <yuntux@gmail.com>
2015-05-02 18:38:19 +0200

    Update according ebicspycommit 77

--- __openerp__.py
+++ __openerp__.py
@@ -28,7 +28,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'templates.xml',
+        #'templates.xml',
 		'views/ebics.xml',
     ],
     # only loaded in demonstration mode
--- models.py
+++ models.py
@@ -10,9 +10,9 @@ class ebics_config(models.Model):
     ###################################################
     ################# LOGGER API STUB #################
     ###################################################
-    def logMessage(self, title, string):
-        print title, string[:1500]
-        self.env['l10n_fr_ebics.ebics_log'].create({'name':title, 'content':string, 'ebics_config_id':self.id})
+    def logMessage(self, level, title, string):
+        print level, title, string[:1500]
+        self.env['l10n_fr_ebics.ebics_log'].create({'name':title, 'level':level, 'content':string, 'ebics_config_id':self.id})
 
     ###################################################
     ################# STORAGE API STUB ################
@@ -28,35 +28,17 @@ class ebics_config(models.Model):
         return str(self.bank_encrypt_key_certificate_hash)
 
     def saveLetter(self, letter, letterType, partnerName, bankName):
-        if letterType == "INI_letter_A005":
-            letterType = "ini_letter_sign"
-        if letterType == "HIA_letter_E002":
-            letterType = "hia_letter_encrypt"
-        if letterType == "HIA_letter_X002":
-            letterType = "hia_letter_auth"
         self.write({letterType : letter.encode('base64')})
 
+    def saveBankKey(self, keyType, keyVersion, owner, modulus, public_exponent, certificate) :
+        self.write({'bank_'+keyType+'_key_certificate': certificate,
+                    'bank_'+keyType+'_key_modulus': str(long(binascii.hexlify(modulus), 16)),
+                    'bank_'+keyType+'_key_public_exponent': str(int(public_exponent, 16)),
+                    'bank_'+keyType+'_key_version': keyVersion})
 
-    def saveBankKeys(self, bankName, auth_certificate, auth_modulus, auth_exponent, auth_version, encrypt_certificate, encrypt_modulus, encrypt_exponent, encrypt_version):
-        self.write({'bank_auth_key_certificate': auth_certificate,
-                    'bank_auth_key_modulus': str(long(binascii.hexlify(auth_modulus), 16)),
-                    'bank_auth_key_public_exponent': str(int(auth_exponent, 16)),
-                    'bank_auth_key_version': auth_version,
-                    'bank_encrypt_key_certificate': encrypt_certificate,
-                    'bank_encrypt_key_modulus': str(long(binascii.hexlify(encrypt_modulus), 16)),
-                    'bank_encrypt_key_public_exponent': str(int(encrypt_exponent, 16)),
-                    'bank_encrypt_key_version': encrypt_version})
- 
-    def saveKey(self, keyType, owner, modulus, private_exponent, public_exponent) :
-        #TODO : found the keyVersion with an other method
-        if keyType == "encrypt" :
-            keyVersion = "E002"
-        elif keyType == "auth" :
-            keyVersion = "X002"
-        elif keyType == "sign":
-            keyVersion = "A005"
-
-        self.write({'partner_'+keyType+'_key_modulus': str(modulus),
+    def savePartnerKey(self, keyType, keyVersion, owner, modulus, private_exponent, public_exponent, certificate) :
+        self.write({"partner_"+keyType+"_key_certificate" : certificate,
+					'partner_'+keyType+'_key_modulus': str(modulus),
                     'partner_'+keyType+'_key_public_exponent': str(public_exponent),
                     'partner_'+keyType+'_key_private_exponent': str(private_exponent),
                     'partner_'+keyType+'_key_version': keyVersion})
@@ -66,28 +48,14 @@ class ebics_config(models.Model):
         res = self.read([targetField])[0][targetField]
         return long(res)
 
-    def getBankKeyComponent(self, bankName, keyComponent, keyVersion):
-        if keyVersion == "E002" :
-            keyType = "encrypt"
-        elif keyVersion == "X002" :
-            keyType = "auth"
+    def getBankKeyComponent(self, bankName, keyComponent, keyType):
         targetField = 'bank_'+keyType+'_key_'+keyComponent
         res = self.read([targetField])[0][targetField]
         return long(res)
 
-    def saveCertificate(self, certificateType, partnerName, content):
-        self.write({"partner_"+certificateType+"_key_certificate" : content})
-
     def loadCertificate(self, certificateType, partnerName):
-        # TODO : check if the return value is correct, text field may no be splited in lines
         targetField = 'partner_'+certificateType+'_key_certificate'
-        f = str(self.read([targetField])[0][targetField])
-        certificate = ""
-        for line in f :
-            certificate += line.strip()
-        certificate = str(certificate).replace('-----BEGINCERTIFICATE-----', '').replace('-----ENDCERTIFICATE-----', '')
-        return certificate
-                   
+        return str(self.read([targetField])[0][targetField])
 
     ###################################################
     ############### ODOO OBJECT FUNCTIONS #############
@@ -96,20 +64,27 @@ class ebics_config(models.Model):
         bank = Bank(self, str(self.bank_name), str(self.bank_host), str(self.bank_port), str(self.bank_root), str(self.bank_host_id))
         partner = Partner(self, str(self.company_id.name), str(self.partner_id), str(self.user_id), self)
         partner.loadPartnerKeys()
-        partner.loadBankKeys(bank, str(self.bank_encrypt_key_version), str(self.bank_encrypt_key_version))
+        partner.loadBankKeys(bank)
         print "========== PARTNER KEYS AND CERTIFICATES LOADED =========="
         print "========== BANK KEYS AND CERTIFICATES LOADED =========="
         return partner,bank
 
+    def hook_pre_send_file(self, partner, bank, dic):
+        #fileUpload_from_fileSystem(partner, bank, "/home/yuntux/helloWorld.mp3","pain.xxx.cfonb160.dct", "fileName", True)  
+        fileUpload_from_fileSystem(partner, bank, "/home/yuntux/helloWorld.txt","pain.xxx.cfonb160.dct", "fileName", True)  
+
+    def hook_post_get_file(self, partner, bank, dic):
+        fileDownload_to_fileSystem(partner, bank, "/home/yuntux/")
+
     @api.one
     def send_file(self):
         partner,bank = self.init_connexion()
-        fileUpload_from_fileSystem(partner, bank, "/home/yuntux/helloWorld.mp3","pain.xxx.cfonb160.dct", "fileName", True)  
+        self.hook_pre_send_file(partner, bank, None)
 
     @api.one
     def get_file(self):
         partner,bank = self.init_connexion()
-        fileDownload_to_fileSystem(partner, bank, "/home/yuntux/")
+        self.hook_post_get_file(partner, bank, None)
 
     @api.one
     def send_partner_keys(self):
@@ -201,7 +176,8 @@ class ebics_config(models.Model):
 class ebics_log(models.Model):
     _name = 'l10n_fr_ebics.ebics_log'
     name = fields.Char()
-    ebics_config_id = fields.Many2one('l10n_fr_ebics.ebics_config', "EBICS Configuration", required=True)
+    ebics_config_id = fields.Many2one('l10n_fr_ebics.ebics_config', "EBICS Configuration", required=True, ondelete='cascade')
     content = fields.Text(required=True)
+    level = fields.Text(required=True)
 
 # vim:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
